4f03ee21b25991751d255dcff0b8fe9945e9d756
KassandraSaint <kassandrastripper@gmail.com>
2022-06-19 14:41:54 -0400

    Add project files.

---
+++ CardDealer/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace CardDealer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numCardsBox = new System.Windows.Forms.TextBox();
+            this.dealBtn = new System.Windows.Forms.Button();
+            this.dealtCardsBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.valueLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cardsLeftLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Cards to Deal:";
+            // 
+            // numCardsBox
+            // 
+            this.numCardsBox.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numCardsBox.Location = new System.Drawing.Point(457, 49);
+            this.numCardsBox.Name = "numCardsBox";
+            this.numCardsBox.Size = new System.Drawing.Size(136, 39);
+            this.numCardsBox.TabIndex = 1;
+            this.numCardsBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dealBtn
+            // 
+            this.dealBtn.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealBtn.Location = new System.Drawing.Point(226, 114);
+            this.dealBtn.Name = "dealBtn";
+            this.dealBtn.Size = new System.Drawing.Size(198, 50);
+            this.dealBtn.TabIndex = 2;
+            this.dealBtn.Text = "Deal Cards";
+            this.dealBtn.UseVisualStyleBackColor = true;
+            this.dealBtn.Click += new System.EventHandler(this.dealBtn_Click);
+            // 
+            // dealtCardsBox
+            // 
+            this.dealtCardsBox.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealtCardsBox.FormattingEnabled = true;
+            this.dealtCardsBox.ItemHeight = 29;
+            this.dealtCardsBox.Location = new System.Drawing.Point(106, 206);
+            this.dealtCardsBox.Name = "dealtCardsBox";
+            this.dealtCardsBox.Size = new System.Drawing.Size(487, 265);
+            this.dealtCardsBox.TabIndex = 3;
+            this.dealtCardsBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(193, 500);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Value of Hand:";
+            // 
+            // valueLabel
+            // 
+            this.valueLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.valueLabel.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueLabel.Location = new System.Drawing.Point(418, 491);
+            this.valueLabel.Name = "valueLabel";
+            this.valueLabel.Size = new System.Drawing.Size(175, 46);
+            this.valueLabel.TabIndex = 5;
+            this.valueLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(140, 576);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(251, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cards Left in Deck:";
+            // 
+            // cardsLeftLabel
+            // 
+            this.cardsLeftLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardsLeftLabel.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardsLeftLabel.Location = new System.Drawing.Point(418, 575);
+            this.cardsLeftLabel.Name = "cardsLeftLabel";
+            this.cardsLeftLabel.Size = new System.Drawing.Size(175, 46);
+            this.cardsLeftLabel.TabIndex = 7;
+            this.cardsLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.dealBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 646);
+            this.Controls.Add(this.cardsLeftLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.valueLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dealtCardsBox);
+            this.Controls.Add(this.dealBtn);
+            this.Controls.Add(this.numCardsBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Card Dealer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numCardsBox;
+        private System.Windows.Forms.Button dealBtn;
+        private System.Windows.Forms.ListBox dealtCardsBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label valueLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label cardsLeftLabel;
+    }
+}
+
---
+++ CardDealer/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardDealer
+{
+    public partial class Form1 : Form
+    {
+        // Creating dictionary deck to hold all cards
+        private Dictionary<string, int> deck = new Dictionary<string, int>()
+        {
+            // Spades
+            {"Ace of Spades",1}, {"2 of Spades",2}, {"3 of Spades",3},
+            {"4 of Spades",4}, {"5 of Spades",5}, {"6 of Spades",6},
+            {"7 of Spades",7}, {"8 of Spades",8}, {"9 of Spades",9},
+            {"10 of Spades",10}, {"Jack of Spades",10}, {"Queen of Spades",10}, {"King of Spades",10},
+
+            // Hearts
+            {"Ace of Hearts",1}, {"2 of Hearts",2}, {"3 of Hearts",3},
+            {"4 of Hearts",4}, {"5 of Hearts",5}, {"6 of Hearts",6},
+            {"7 of Hearts",7}, {"8 of Hearts",8}, {"9 of Hearts",9},
+            {"10 of Hearts",10}, {"Jack of Hearts",10}, {"Queen of Hearts",10}, {"King of Hearts",10},
+
+            // Clubs
+            {"Ace of Clubs",1}, {"2 of Clubs",2}, {"3 of Clubs",3},
+            {"4 of Clubs",4}, {"5 of Clubs",5}, {"6 of Clubs",6},
+            {"7 of Clubs",7}, {"8 of Clubs",8}, {"9 of Clubs",9},
+            {"10 of Clubs",10}, {"Jack of Clubs",10}, {"Queen of Clubs",10}, {"King of Clubs",10},
+
+            // Diamonds
+            {"Ace of Diamonds",1}, {"2 of Diamonds",2}, {"3 of Diamonds",3},
+            {"4 of Diamonds",4}, {"5 of Diamonds",5}, {"6 of Diamonds",6},
+            {"7 of Diamonds",7}, {"8 of Diamonds",8}, {"9 of Diamonds",9},
+            {"10 of Diamonds",10}, {"Jack of Diamonds",10}, {"Queen of Diamonds",10}, {"King of Diamonds",10}
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Updating number of cards in the deck
+            cardsLeftLabel.Text = deck.Count.ToString();
+        }
+
+        private void dealBtn_Click(object sender, EventArgs e)
+        {
+            // Initializing some variables to hold number of cards to deal and dealt
+            int toDeal = 0;
+            int cardsVal = 0;
+            // Initializing Random Object
+            Random rand = new Random();
+
+            // Clearing content of a List Box
+            dealtCardsBox.Items.Clear();
+
+            // Clearing value of cards on hand
+            valueLabel.Text = "";
+
+            // Checking if user entered right input for cards to deal
+            if (int.TryParse(numCardsBox.Text, out toDeal))
+            {
+                // Checking of entered number is greater than 0 but less than # of cards in a deck
+                if (toDeal > 0 && toDeal <= deck.Count)
+                {
+                    // Dealing card randomly
+                    for (int count = 0; count < toDeal; count++)
+                    {
+                        // Get random number to select a card
+                        int randNum = rand.Next(deck.Count);
+
+                        // Getting a card at a random position and storing in the card variable
+                        var card = deck.ElementAt(randNum);
+                        // Displaying the picked card in the List Box
+                        dealtCardsBox.Items.Add(card.Key);
+
+                        // Updating and displaying value of cards on hand
+                        cardsVal += card.Value;
+                        valueLabel.Text = cardsVal.ToString();
+
+                        // Removing picked card from the deck
+                        deck.Remove(card.Key);
+                        // Updating number of card in a deck
+                        cardsLeftLabel.Text = deck.Count.ToString();
+                    }
+                }
+                else
+                {
+                    // If deck has no more cards showing the message
+                    if (deck.Count == 0)
+                    {
+                        MessageBox.Show("The deck is empty!");
+                    }
+                    else
+                    {
+                        // Showing a message if user entered a number which is out of the range of deck cards
+                        MessageBox.Show("Enter a number in the range of 1 through " + deck.Count.ToString());
+                    }
+                }
+            }
+            else
+            {
+                // Showing a message if user's input was invalid
+                MessageBox.Show("Enter an integer!");
+            }
+        }
+    }
+}
